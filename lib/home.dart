dd1d9d1f7e830c8471e0e6336840e15698d0878c
Sam Victor <iamvictorsam@gmail.com>
2023-07-31 17:36:36 +0100

    demo

---
+++ lib/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import 'items.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shimmer Effect'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildProduct(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProduct() => GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 6,
+        itemBuilder: (context, index) => Shimmer.fromColors(
+            baseColor: Colors.black54,
+            highlightColor: Colors.black12,
+            direction: ShimmerDirection.ttb,
+            period: const Duration(milliseconds: 900), // added period
+            child: const Items()),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          childAspectRatio: 1 / 1.7,
+        ),
+      );
+}
+
+// Shimmer.fromColors(
+// baseColor: Colors.black54,
+// highlightColor: Colors.purple,
+// direction: ShimmerDirection.ttb,
+// enabled: true,
+// loop: 2,
+// period: const Duration(seconds: 3),
+// child: _buildProduct(),
+// )
---
+++ lib/items.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Items extends StatelessWidget {
+  const Items({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            height: 190,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.teal,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              'Product',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 5),
+            Icon(Icons.bookmark_add),
+          ],
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          '₦2,000.00',
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+        ),
+      ],
+    );
+  }
+}
